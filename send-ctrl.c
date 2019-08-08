5788e3ff89e77c553cb9f9472c7a76a7ad4278f2
ruichun <ruichun@illinois.edu>
2019-08-08 14:11:52 -0500

    add data folder

--- send-ctrl.c
+++ send-ctrl.c
@@ -3,6 +3,14 @@
 int main()
 {
     cmd("sshpass -p \"raspberry\" scp ctrl.txt pi@192.168.1.15:/home/pi/routing-control/ctrl.txt");
-    
+    // cmd("sshpass -p \"raspberry\" scp ctrl.txt pi@192.168.1.15:/home/pi/routing-control/ctrl.txt");
+    // cmd("sshpass -p \"raspberry\" scp ctrl.txt pi@192.168.1.15:/home/pi/routing-control/ctrl.txt");
+    // cmd("sshpass -p \"raspberry\" scp ctrl.txt pi@192.168.1.15:/home/pi/routing-control/ctrl.txt");
+    // cmd("sshpass -p \"raspberry\" scp ctrl.txt pi@192.168.1.15:/home/pi/routing-control/ctrl.txt");
+    // cmd("sshpass -p \"raspberry\" scp ctrl.txt pi@192.168.1.15:/home/pi/routing-control/ctrl.txt");
+    // cmd("sshpass -p \"raspberry\" scp ctrl.txt pi@192.168.1.15:/home/pi/routing-control/ctrl.txt");
+    // cmd("sshpass -p \"raspberry\" scp ctrl.txt pi@192.168.1.15:/home/pi/routing-control/ctrl.txt");
+    // cmd("sshpass -p \"raspberry\" scp ctrl.txt pi@192.168.1.15:/home/pi/routing-control/ctrl.txt");
+    // cmd("sshpass -p \"raspberry\" scp ctrl.txt pi@192.168.1.15:/home/pi/routing-control/ctrl.txt");
     return 0;
 }
