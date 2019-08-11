2e6e0a6d89f645586034206f2ae05fda9153b217
ruichun <ruichun@illinois.edu>
2019-08-11 14:09:25 -0500

    try ad-hoc network

---
+++ setup-adhoc.c
@@ -0,0 +1,31 @@
+#include "rc.h"
+
+int main()
+{
+    char cmdBuff[BUFSIZ];
+
+    // set type as ibss
+    cmd("sudo ifconfig wlan0 down");
+    cmd("sudo iw wlan0 set type ibss");
+    cmd("sudo ifconfig wlan0 up");
+
+    //set ip addr
+    memset(cmdBuff,0,sizeof(char)*BFSIZE);
+    strcat(cmdBuff,"sudo ifconfig wlan0 ");
+    strcat(cmdBuff,LOCALIP);
+    cmd(cmdBuff);
+
+    // set tx power
+    cmd("sudo iw wlan0 set txpower fixed 0");
+
+    // join or create a ad-hoc net
+    cmd("sudo iw wlan0 ibss join RaspAdhoc freq 2442");
+
+    // set eth0 ip for ssh
+    memset(cmdBuff,0,sizeof(char)*BFSIZE);
+    strcat(cmdBuff,"sudo ifconfig eth0 ");
+    strcat(cmdBuff,ETH0IP);
+    cmd(cmdBuff);
+
+    return 0;
+}
